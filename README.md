7fbd7019a12bc9f7e565377b06bb38adc244983b
damianrivas <drivas1993@gmail.com>
2019-04-29 17:54:19 -0700

    Add timer and simple RNG

---
+++ README.md
@@ -0,0 +1,3 @@
+# Damian's C++ Library
+
+This is my library of personally made C++ headers that I will work on either for fun or for serious use.
---
+++ timer.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <chrono>
+#include <string>
+
+// Use the Object Lifetime Paradigm
+// The timer begins as soon as an instance is produced.
+struct Timer
+{
+    std::chrono::time_point<std::chrono::steady_clock> start, end;
+    std::chrono::duration<float> duration;
+    std::string operation;
+
+    Timer() : operation("Timer"), start(std::chrono::high_resolution_clock::now()) {}
+
+    Timer(std::string operation)
+        : operation(operation), start(std::chrono::high_resolution_clock::now()) {}
+
+    // Display results at the end of the instance's lifetime, in milliseconds.
+    ~Timer()
+    {
+        end = std::chrono::high_resolution_clock::now();
+        duration = end - start;
+
+        float ms = duration.count() * 1000.0f;
+        std::cout << operation << " took " << ms << "ms " << std::endl;
+    }
+};
